f01fec1e7810500aa5c8d3fc4507690e69ff1ef4
Efren Carrillo <efren.sistemas@gmail.com>
2019-02-15 12:21:15 -0500

    Pasando funciones a los props

--- src/App.js
+++ src/App.js
@@ -10,12 +10,21 @@ class App extends Component {
       { name : 'Gaby', age: 19 },
       { name : 'Danny', age: 25 },
       { name : 'Efren', age: 24 }
-    ]
+    ],
+    otherState: 'tipo candena'
   }
 
   //declarar un función para botón (arrow function)
-  switchNameHandler = () => {
-    console.log("Was clicked")
+  switchNameHandler = (namePerson) => { // ocurrira en el evento click
+    // console.log("Was clicked")
+    // NO UTILICES ESO PARA CAMBIAR AL STATE this.state.persons[2].name = "Eduardo"
+    this.setState({ // utilizamos setState para cambiar un state y recibe un objeto
+      persons: [ //copiamos el array y cambiamos solo donde queremos
+        { name : 'Gaby', age: 19 },
+        { name : 'Danny', age: 25 },
+        { name : namePerson, age: 24 } // cambie Efren a Eduardo
+      ]
+    })
   }
 
   render() {
@@ -23,12 +32,23 @@ class App extends Component {
       <div className="App">
         <p>Trabajando con Reactjs</p>
         {/* No uses los parentesis al llamarlo lo ejecuras si haces eso */}
-        <button onClick={this.switchNameHandler}>Cambiar de Nombre</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >
+        <button onClick={() => this.switchNameHandler('Eduardo') }>Cambiar de Nombre</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+        />
+        {/* Ahora cuando de click aqui se cambiará el item 2 */}
+        <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this,"Isai!")}
+        >
           Hobbies: Tocar guitarra
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} 
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = ( props ) => {
     return (
         <div>
-            <p>Soy {props.name} y tengo {props.age} años!</p>
+            <p onClick={props.click}>Soy {props.name} y tengo {props.age} años!</p>
             {/* Con children yo puedo incluir lo que está dentro de App.js "hobbies" */}
             <p>{props.children}</p>
         </div>
