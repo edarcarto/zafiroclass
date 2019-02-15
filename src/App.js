79c69474ab340945b7758fe9749c3f6ea667b6b1
Efren Carrillo <efren.sistemas@gmail.com>
2019-02-15 15:29:16 -0500

    Listas Flexibles

--- src/App.js
+++ src/App.js
@@ -6,23 +6,40 @@ class App extends Component {
   // llamar a state es una variable especial de Component
   // state es un objeto y se le pueden agregar elementos
   state = {
+    // agrefamos id
     persons: [ //agregare persona
-      { name: 'Gaby', age: 19 },
-      { name: 'Danny', age: 25 },
-      { name: 'Efren', age: 24 }
+      { id:'001' , name: 'Gaby', age: 19 },
+      { id:'002' , name: 'Danny', age: 25 },
+      { id:'003' , name: 'Efren', age: 24 }
     ],
     otherState: 'tipo candena',
     showPersons: false
   }
 
-  //crearemos una otra funcion para cambiar el item 1 por medio de un input 
-  nameChangedHandler = (event) => {
+  //Ahora esta función cambiara los nombres de cada elemento cuando
+  // escribas algo en el input
+  nameChangedHandler = (event, id) => {
+    // se agrego el evento y un id
+    const personIndex = this.state.persons.findIndex(p => {
+      // buscas el index y si id es igual p.id retornas el objeto
+      return p.id === id
+    });
+    // el objeto es mutable (spred) asi que lo asignamos un objeto 
+    // cuando queremos editarlo
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    // en caso no usemos ... podemos hacerlo de la siguiente manera
+    // const person = Object.assign({}, this.state.persons[personIndex])
+    // cambiamos el mombre que viene en el event target
+    person.name = event.target.value;
+    // actualizamos el array state que es mutable
+    const persons = [...this.state.persons];
+    // cambiamos exactamente el indice modificado
+    persons[personIndex] = person;
+    // set state actualizamos el state
     this.setState({
-      persons: [
-        { name: 'Gaby', age: 19 },
-        { name: event.target.value, age: 25 }, //event.target.value estoy capturando valor escrito en el input
-        { name: 'Efren', age: 24 }
-      ]
+      persons: persons
     })
   }
 
@@ -36,6 +53,17 @@ class App extends Component {
   deletePersonHandler = (personIndex) =>{
     // almaceno el state en una variable
     const persons = this.state.persons
+    // funcion splice elimina un item de array segun el index
+    persons.splice(personIndex, 1);
+    // actualizo el state
+    this.setState({persons : persons});
+  }
+
+  //Eliminando un array inmutable
+  deletePersonHandler = (personIndex) =>{
+    // almaceno el state en una variable
+    // const persons = this.state.persons
+    const persons = [...this.state.persons]
     // funcion splice elimina un item de array segun el index
     persons.splice(personIndex, 1);
     // actualizo el state
@@ -56,6 +84,9 @@ class App extends Component {
     if (this.state.showPersons) {
       // si showPersons es verdad inserto HTML sino es nulo
       // recorremos el estado persons con map y retornamos el componente
+      // agregamos una key para borrar el error de consola
+      // la documentacion dice que cada componente dentro de un array
+      // recorrido debe tener key
       persons = (
         <div >
           {this.state.persons.map((person, index) => {
@@ -63,6 +94,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
+              key={person.id}
+              changed={ (event) => this.nameChangedHandler(event, person.id ) }
             />
           })}
         </div>
