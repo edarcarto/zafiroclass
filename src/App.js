c00bfbdd5c3effc9695bafd68495aeb8a022b5c3
Efren Carrillo <efren.sistemas@gmail.com>
2019-02-15 11:04:19 -0500

    Manejando props y states

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  // llamar a state es una variable especial de Component
+  // state es un objeto y se le pueden agregar elementos
+  state = {
+    persons: [ //agregare persona
+      { name : 'Gaby', age: 19 },
+      { name : 'Danny', age: 25 },
+      { name : 'Efren', age: 24 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <p>Trabajando con Reactjs</p>
-        <Person name="Gaby" age="19" />
-        <Person name="Danny" age="25">
-          Hobbies: : Tocar guitarra
+        <button>Cambiar de Nombre</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >
+          Hobbies: Tocar guitarra
         </Person>
-        <Person name="Efren" age="24"/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const person = (props) => {
-    return <p>Soy {props.name} y tengo {props.age} años!</p>
+const person = ( props ) => {
+    return (
+        <div>
+            <p>Soy {props.name} y tengo {props.age} años!</p>
+            {/* Con children yo puedo incluir lo que está dentro de App.js "hobbies" */}
+            <p>{props.children}</p>
+        </div>
+    )
 }
 
 export default person;
