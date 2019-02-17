ed932f20786fbc722523b1c5b86f93a0c0fd8096
Efren Carrillo <efren.sistemas@gmail.com>
2019-02-17 10:52:14 -0500

    [FIX] Agregando media querys con module classes

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,8 +5,6 @@ const person = ( props ) => {
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>Soy {props.name} y tengo {props.age} años!</p>
-            {/* Con children yo puedo incluir lo que está dentro de App.js "hobbies" */}
-            <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name }/>
         </div>
     )
