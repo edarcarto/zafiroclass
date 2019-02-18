52c4550bb0638de06d92715e0a711fb198b159d5
Efren Carrillo <efren.sistemas@gmail.com>
2019-02-17 23:11:36 -0500

    [HOTFIX] useEffect segundo parametro

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,8 +4,15 @@ import classes from './Cockpit.css';
 const cockpit = (props) => {
     // agregando el cliclo de vida useEffect para componentes funcionales
     useEffect(() => {
+        // por lo general es para manejar http request
         console.log('[cockpit.js] useEffect')
-    })
+        // ejemplo "simular un http request"
+        setTimeout(() => {
+            alert('Saved data to cloud');
+        },1000); // 1000 milisegundos
+    },[]); // acepta un segundo parametro de tipo array [props.persons]
+
+    // useEffect(); puedes llamar muchos mas
 
     let asignClasses = [];
     let btnClass = '';
